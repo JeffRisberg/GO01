870768bd4158f0586a7aeba7dee63e065f21c761
JeffRisberg <jrisberg@aol.com>
2019-05-15 21:54:58 -0700

    first commit

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	var count = 1
 
-	if (count == 0) {
+	if count == 0 {
 		fmt.Println("Hello World")
 	} else {
 		fmt.Println("Goodbye World")
 	}
+
+	sum := 0
+	for i := 1; i < 5; i++ {
+		sum += i
+	}
+	fmt.Println(sum) // 10 (1+2+3+4)
 }
