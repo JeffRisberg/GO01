185e7bd1df049bca8d05c160193a04e8bfc031d9
JeffRisberg <jrisberg@aol.com>
2019-05-26 23:48:29 -0700

    type checking example

--- main.go
+++ main.go
@@ -41,7 +41,21 @@ func main() {
 	cardValue := newCardValue()
 
 	fmt.Println(card)
-	fmt.Print(cardValue)
+	fmt.Println(cardValue)
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
 
 func newCardValue() int {
